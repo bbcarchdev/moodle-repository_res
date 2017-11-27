13fc8fbbe160fe8e976169f5166c767d45d57c0a
Elliot Smith <elliot.smith@bbc.co.uk>
2017-11-27 15:21:48 +0000

    Bump version for release 0.0.5

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'repository_res';
 $plugin->requires = 2017051500;
-$plugin->version = 2017112700;
+$plugin->version = 2017112701;
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.0.4';
+$plugin->release = '0.0.5';
